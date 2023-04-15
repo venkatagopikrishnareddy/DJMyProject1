f8ec4a2fffb293cda8bdf04e26a6cc10bf2ce083
venkatagopikrishna <Djangopycharm1@gmail.com>
2023-04-15 20:49:06 +0530

    Initial commit

---
+++ MyApps1/views.py
@@ -0,0 +1,9 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+def display(request):
+    s="<h1>Hello User ,Welcome to Django First  Project (DJMyProject1)&First-App(MyApps1)</h1><hr/>"
+    return HttpResponse(s)
+def show(request):
+    ss='<center><h1>Hello User,Welcome to Django FirstProject (DJMyproject) & First-App (MyApp1) </h1></center><hr/>'
+    return HttpResponse(ss)
+
