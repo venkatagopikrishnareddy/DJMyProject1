accff10145d6b17985ba16a21ee175cd09f87817
venkatagopikrishna <Djangopycharm1@gmail.com>
2023-04-15 21:05:41 +0530

    this change in pycharm

--- MyApps1/views.py
+++ MyApps1/views.py
@@ -4,6 +4,6 @@ def display(request):
     s="<h1>****Hello User ,Welcome to Django First  Project (DJMyProject1)&First-App(MyApps1)****</h1><hr/>"
     return HttpResponse(s)
 def show(request):
-    ss='<center><h1>Hello User,Welcome to Django FirstProject (DJMyproject) & First-App (MyApp1) </h1></center><hr/>'
+    ss='<center>$$$<h1>Hello User,Welcome to Django FirstProject (DJMyproject) & First-App &GOPI (MyApp1)$$$ </h1></center><hr/>'
     return HttpResponse(ss)
 
